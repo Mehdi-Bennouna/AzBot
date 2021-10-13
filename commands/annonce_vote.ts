7181bd6e8b7c6fc82a3064452a92a85404baf06e
BennounaMehdi <Bennouna.Mehdi@hotmail.com>
2021-10-13 14:56:08 +0100

    added command annonce_vote

---
+++ commands/annonce_vote.ts
@@ -0,0 +1,38 @@
+import { ICommand } from "wokcommands";
+
+export default {
+  category: "Announcement",
+  description: "Annonce un vote",
+  permissions: ["ADMINISTRATOR"],
+  slash: true,
+  testOnly: true,
+
+  options: [
+    {
+      name: "contenu",
+      description: "contenu de l'annonce",
+      type: 3,
+      required: true,
+    },
+  ],
+
+  callback: ({ interaction, channel }) => {
+    channel.send({
+      embeds: [
+        {
+          title: "Annonce Vote",
+          description: interaction.options.getString("contenu")!,
+          fields: [
+            {
+              name: "\u200b",
+              value:
+                "Veuiller faire votre choix dans le channel <#897433309842378762>",
+            },
+          ],
+        },
+      ],
+    });
+
+    interaction.reply("done");
+  },
+} as ICommand;
